d1912823c1a38c7cfb97f18a3c6794b853fc3c93
Mahao <mahao@iot.chinamobile.com>
2017-03-27 17:15:48 +0800

    修改相关文件

--- public/js/front_end/index.js
+++ public/js/front_end/index.js
@@ -16,8 +16,20 @@ $(function () {
                 pagination: '.swiper-pagination',
                 // 如果需要前进后退按钮
                 nextButton: '.swiper-button-next',
-                prevButton: '.swiper-button-prev',
-            })
+                prevButton: '.swiper-button-prev'
+            });
+	        
+	        // 产品中心swiper初始化
+	        var productSwiper = new Swiper('.product-content-container',{
+		        slidesPerView : 3,
+		        slidesPerGroup : 3,
+		        paginationClickable: true,
+		        // 如果需要分页器
+		        // pagination: '.productSwiper-pagination',
+		        // 如果需要前进后退按钮
+		        nextButton: '.swiper-button-next',
+		        prevButton: '.swiper-button-prev'
+	        })
         },
         bindEvent: function () {
         }

0ac54440d3a97d6730892635d6e62a19dedac74d
Mahao <mahao@iot.chinamobile.com>
2017-04-07 17:01:24 +0800

    修改相关文件

---
+++ public/js/back_end/login.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Mahao on 2017/4/7.
+ */
+$(function(){
+
+    // 登录
+    $('#login').on('submit', function(e){
+        e.preventDefault();
+        var $form = $(this),
+            form = $form.attr("from-data");
+        $.post("/admin/login", $form.serialize(), function(res){
+            if(res.code == 0){
+
+                //处理成功时的响应
+                if(form) {
+                    location.href = form;
+                } else {
+                    location.href = '../admin/index';
+                }
+            }else{
+
+                // 返回失败信息
+                IOT.tips(res.msg, "error", 1000);
+            }
+        }, 'json');
+    })
+});
---
+++ routes/back_end/index.js
@@ -0,0 +1,54 @@
+/**
+ * Created by Mahao on 2017/4/7.
+ */
+import controller from '../tools/controllers'
+export default class extends controller {
+    renders() {
+
+        // 登录
+        this.router.get('/admin/login', async(ctx, next) => {
+
+            // 判断是否是debug
+            var debug = ctx.request.query._d;
+            if (debug == 1) {
+                ctx.body = ctx.state;
+                return false;
+            }
+            await ctx.render('./back_end_jade/login')
+        });
+
+        // 后台管理-首页
+        this.router.get('/admin/index', async(ctx, next) => {
+
+            // 判断是否是debug
+            var debug = ctx.request.query._d;
+            if (debug == 1) {
+                ctx.body = ctx.state;
+                return false;
+            }
+            await ctx.render('./back_end_jade/index')
+        });
+    }
+
+    actions() {
+
+        // 登录验证
+        this.router.post('/admin/login', async(ctx, next) => {
+            let userName = ctx.request.body.userName;
+            let userPassw = ctx.request.body.pwd;
+            if (userName === 'admin' && userPassw==='cqyir') {
+                ctx.session.user = 'admin'; // 保留至session中
+                ctx.body = {
+                    "code": 0,
+                    "msg": "账号验证成功"
+                }
+            } else {
+                ctx.body = {
+                    "code": 200,
+                    "msg": "账号或密码错误"
+                }
+            }
+
+        });
+    }
+}
